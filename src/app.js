9097cafd4bee386e42520f9253eeb614f9af92f4
shameer <shameer.myd@gmail.com>
2025-11-29 17:14:10 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,37 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    console.log(res.send("Hello route.., You are from Server"));
+app.get("/user",(req,res)=>{
+    res.send({ firstName: "Muhammed", lastName: "Shameer" })
 })
 
-app.use("/test",(req,res)=>{
-    console.log(res.send("Test route.. "))
+app.post("/user",(req,res)=>{
+    res.send("Data added Successfully Saved")
 })
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted Successfully ")
+})
+
+app.use("/user",(req,res)=>{
+    res.send("Hello from server")
+})
+
+// app.use("/hello/2",(req,res)=>{
+//     console.log(res.send("Hello/2 route.."));
+// })
+
+// app.use("/hello",(req,res)=>{
+//     console.log(res.send("Hello route.., You are from Server"));
+// })
+
+// app.use("/test",(req,res)=>{
+//     console.log(res.send("Test route.. "))
+// })
+
+// app.use("/",(req,res)=>{
+//     console.log(res.send("Main page "))
+// })
 
 app.listen(3333,()=>{
     console.log("server port: 3333 running...");
