0944978f8dca86e4598a19e43e3207cc18b8fd25
shameer <shameer.myd@gmail.com>
2025-12-04 12:49:26 +0530

    DB Connection After app server start - basic

--- src/app.js
+++ src/app.js
@@ -1,30 +1,8 @@
+//page: app.js
 const express = require("express");
+require("./config/database.js")
 const app = express();
-// const {adminAuth,userAuth} = require("./middlewares/auth.js")
 
-// app.use("/admin",adminAuth);
-
-app.use("/",(err,req,res,next)=>{
-    res.status(500).send("Something went wrong 1st!!")
-});
-
-app.get("/getData",(res,req)=>{        
-    throw new Error("dddddd"); //created a manul error, while calling '/getData'
-});
-
-app.use("/",(err,req,res,next)=>{
-    res.status(500).send("Something went wrong 2nd!!")
-});
-
-
-
-// app.get("/admin/profile",(req,res)=>{
-//     res.send({ auth:"admin", firstName: "Muhammed", lastName: "Shameer" });
-// });
-
-// app.get("/user/profile",userAuth,(req,res)=>{
-//     res.send({ auth:"user", firstName: "Muhammed", lastName: "Shameer" });
-// });
 
 app.listen(3333,()=>{
     console.log("server port: 3333 running...");
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+//page : config/database.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://shameermyd:shameer007@shameerCluster.08t6wgy.mongodb.net");
+    } catch (error) {
+        console.log(error,"Connection Error!");
+    }
+};
+
+connectDB()
+    .then(() => { console.log("✅ DB Connected Successfully ") })
+    .catch((err) => { console.log("❌ DB ConnectionFailed!!") });
