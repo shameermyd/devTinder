cfafea4241b951186499fc7e4be875883d198c0b
shameer <shameer.myd@gmail.com>
2025-12-04 09:03:10 +0530

    Error Handling method basic

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const express = require("express");
-const {adminAuth,userAuth} = require("./middlewares/auth.js")
 const app = express();
+// const {adminAuth,userAuth} = require("./middlewares/auth.js")
 
-app.use("/admin",adminAuth);
+// app.use("/admin",adminAuth);
 
-app.get("/admin/profile",(req,res)=>{
-    res.send({ auth:"admin", firstName: "Muhammed", lastName: "Shameer" });
+app.use("/",(err,req,res,next)=>{
+    res.status(500).send("Something went wrong 1st!!")
 });
 
-app.get("/user/profile",userAuth,(req,res)=>{
-    res.send({ auth:"user", firstName: "Muhammed", lastName: "Shameer" });
+app.get("/getData",(res,req)=>{        
+    throw new Error("dddddd"); //created a manul error, while calling '/getData'
 });
+
+app.use("/",(err,req,res,next)=>{
+    res.status(500).send("Something went wrong 2nd!!")
+});
+
+
+
+// app.get("/admin/profile",(req,res)=>{
+//     res.send({ auth:"admin", firstName: "Muhammed", lastName: "Shameer" });
+// });
+
+// app.get("/user/profile",userAuth,(req,res)=>{
+//     res.send({ auth:"user", firstName: "Muhammed", lastName: "Shameer" });
+// });
 
 app.listen(3333,()=>{
     console.log("server port: 3333 running...");
